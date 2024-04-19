051a7d99463ebba249ee4dad5c45f0613b460821
Takik201 <157398801+Takik201@users.noreply.github.com>
2024-04-19 05:34:41 +0000

    part1

---
+++ part1/02_datatype.js
@@ -0,0 +1,28 @@
+"use strict"// treat all js code as new version
+
+// alert(3+3);
+
+// console.log(3+3); // code readability should be high
+
+console.log("Mangesh");
+
+let name = "mangesh";
+let age = 22;
+let isLoggin = false;
+let state;
+
+// number =>  2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => stand alone value
+// undefiner =>
+// symbol = unique
+
+// object
+
+// k.gawande87@gmail.com
+
+console.log(typeof undefined);
+console.log(typeof null);
+
