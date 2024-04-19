1dcba3cdcc6bd1bf665462e4f630a3bb96a44923
Takik201 <157398801+Takik201@users.noreply.github.com>
2024-04-19 04:54:09 +0000

    learnjs

---
+++ lernjs1.js
@@ -0,0 +1,15 @@
+// console.log("Hi mangesh");
+
+const accoundId= 12345;
+let mail= "mangesh@gmail.com";
+let password = 12345;
+accountCity = "Pune";
+let accountState;
+
+// accoundId = 2 are not allowed
+
+mail= "takik@gmail.com";
+password = 54321;
+accountCity = "Beed";
+
+console.table([accoundId, mail, password, accountCity, accountState]);
